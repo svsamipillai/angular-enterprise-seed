04934f356c0ceaf7c13b5f548474ecf70ecfdd31
rob <rob@rob-xps.(none)>
2013-01-12 23:51:09 -0800

    add playground concept

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,24 +4,26 @@
 
 var directives = angular.module('myApp.directives', []);
 
-
-// set element text as "version"
-directives.directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
-
-
 // causes element to css animate in/out
 directives.directive('fadeIn', function() {
     return {
       compile: function(elm) {
-        console.log('compiling');
         $(elm).css('opacity', 0.1);
         return function(scope, elm, attrs) {
           $(elm).animate({ opacity : 1.0 }, 500 );
         };
       }
+    };
+});
+
+directives.directive('myJqueryPluginUploader', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, elem, attr, ctrl) {
+            // elem is a jQuery lite object
+            // or a jQuery object if jQuery is present.
+            // so call whatever plugins you have.
+            elem.pluginUploadCall();
+        }
     };
 });
